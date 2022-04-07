7c5c9755a0eca92287324ce3d61de37156ed489c
verowangari <verowangari34@gmail.com>
2022-04-07 23:31:12 +0300

    creat index page

--- App/urls.py
+++ App/urls.py
@@ -3,9 +3,9 @@ from . import views
 from django.contrib.auth import views as authViews
 from App.views import Signup,Search_users
 urlpatterns=[
-    path('',views.welcome,name = 'welcome'),
+    path('',views.index,name = 'index'),
     path('signup/', Signup, name='signup'),
     path('search_users/',Search_users, name='search_users'),
-    path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'welcome'}, name='logout'),
+    path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'index'}, name='logout'),
     path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
 ]
--- App/views.py
+++ App/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 #................
 # Create your views here.
-def welcome(request):
-    return render(request, 'base.html')
+def index(request):
+    return render(request, 'index.html')
 
 
 def login(request):
