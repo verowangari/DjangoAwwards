238adba4e1b0cc62a050d99d7d74ff7471230b32
verowangari <verowangari34@gmail.com>
2022-04-07 23:41:18 +0300

    initial migrations

--- App/views.py
+++ App/views.py
@@ -22,7 +22,7 @@ def Signup(request):
 			email = form.cleaned_data.get('email')
 			password = form.cleaned_data.get('password')
 			User.objects.create_user(username=username, email=email, password=password)
-			return redirect('welcome')
+			return redirect('index')
 	else:
 		form = SignupForm()
 	
