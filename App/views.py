82b2325b1edda7d66e296f02f29689ef83d86d61
verowangari <verowangari34@gmail.com>
2022-04-08 14:51:22 +0300

    create views for profile

--- App/views.py
+++ App/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.template import loader
 from django.http  import HttpResponse
-from .forms import SignupForm
+from .forms import SignupForm,UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 #................
@@ -50,3 +51,27 @@ def Search_users(request):
         
     else:
         return HttpResponse(template.render())
+    
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile) 
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile') # Redirect back to profile page
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'profile.html', context)
