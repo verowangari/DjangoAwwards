1462a5979efd4605c6b19d8b365b3270aea42674
verowangari <verowangari34@gmail.com>
2022-04-08 14:49:11 +0300

    update forms.py

--- App/forms.py
+++ App/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Profile
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
@@ -49,3 +50,16 @@ class SignupForm(forms.ModelForm):
 		return self.cleaned_data
 
 
+# Create a UserUpdateForm to update username and email
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+# Create a ProfileUpdateForm to update image
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
