b1c3871d7936e723f9823dbcadd073762a934a88
verowangari <verowangari34@gmail.com>
2022-04-09 00:08:18 +0300

    add a path for newpostform

--- App/urls.py
+++ App/urls.py
@@ -5,6 +5,7 @@ from App.views import Signup,Search_users
 urlpatterns=[
     path('',views.index,name = 'index'),
     path('signup/', Signup, name='signup'),
+    path('newpost/', views.NewPost, name='newpost'),
     path('search_users/',Search_users, name='search_users'),
     path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'index'}, name='logout'),
     path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
