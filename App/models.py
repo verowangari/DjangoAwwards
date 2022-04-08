d10783f99f3bc0cac268259ab1b4f1ee865558af
verowangari <verowangari34@gmail.com>
2022-04-08 22:24:09 +0300

    create a post model

--- App/models.py
+++ App/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from PIL import Image
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
@@ -20,3 +21,22 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size) # Resize image
             img.save(self.image.path) # Save it again and override the larger image
+            
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    picture= CloudinaryField('picture')
+    projecturl= models.URLField(max_length=200)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, default='', null=True ,related_name='author')
+    posted= models.DateField(auto_now_add=True )
+
+    def save_posts(self):
+        self.user
+
+    def delete_posts(self):
+        self.delete()    
+
+
+    @classmethod
+    def search_projects(cls, name):
+        return cls.objects.filter(title__icontains=name).all()
