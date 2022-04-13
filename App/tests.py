e9d66ccb481144b62e46637f1d04ec93e6d44da8
verowangari <verowangari34@gmail.com>
2022-04-13 15:52:13 +0300

    add tests

--- App/tests.py
+++ App/tests.py
@@ -1,3 +1,40 @@
+
 from django.test import TestCase
 
-# Create your tests 
+from App.views import profile
+from .models import Profile, Post,Revieww
+
+
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile=Profile(User='Veronica')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.post=Post(title='Project1')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))  
+
+class ReviewwTestClass(TestCase):
+    def setUp(self):
+        self.revieww=Revieww(text='Incredible')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.revieww, Revieww))  
+
+
+        
+
+
+
+
+
+        
+
+
